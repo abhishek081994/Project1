3978b849ada8eab6e83065add36c593162b6d4c4
abhishek081994 <abhishek_singh1994@outlook.com>
2021-04-05 02:55:57 -0400

    My first java file

---
+++ caladd.java
@@ -0,0 +1,19 @@
+public class caladd {
+    public static void main(String[] args) { 
+        Circle c1 = new Circle(2.5f);
+       // c1.radius = 2.5f;
+        Circle c2 = new Circle(3.0f);
+        //c2.radius = 3.0f;
+        
+        System.out.println("Area of Circle 1 " + c1.findArea() + " Area of Circle2 " +c2.findArea());
+    }
+}
+class Circle{
+    public  float radius;
+    public Circle(float radius){
+        this.radius = radius;
+    }
+    public  double findArea(){
+        return 3.14*radius*radius;
+    }
+}
